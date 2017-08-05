e10094434f80b1a81c948dc465ba8e5f3a1d69fe
Philippa Richardson <philippa.richardson@gmail.com>
2017-08-05 12:18:16 +1200

    packet
    Added a packet class

---
+++ packet.py
@@ -0,0 +1,38 @@
+"""
+ Philippa Richardson and Emily Syme
+ 05 August 2017
+ COSC 264 - Introduction to Networking and the Internet
+     """
+
+class packet_head():
+    """
+     Packet head:
+     magic_no   #the magic number of 0x497E
+     
+     pack_type  #distinguishes two packet types: data_packet & acknowledgement_packet
+                #'type' on its own is a python thing already, or this would be 'type'
+     
+     seq_no     #restricted to the values 0 and 1
+     
+     data_len   #number of user bytes carried in this packet
+                #data_len_max #512
+                #data_len_min #0
+                #if data_len is 0 and pack_type is data_packet:
+                    #data_packet is empty
+                    #End of file transmitted to receiver
+                #if data_len is not 0 and pack_type is acknowledgement_packet:
+                    #Drop packet
+     
+     Content:
+     data         #contains actual data, variable length indicated by data_len
+     
+     ##MYSTERY FIELD##
+    """
+    
+    def __init___(self, magic_no, pack_type, seq_no, data_len):
+        self.magic_no = magicno
+        self.pack_type = pack_type
+        self.seq_no = seq_no
+        self.data_len = data_len
+        #self.mystery_field = a mystery
+        
